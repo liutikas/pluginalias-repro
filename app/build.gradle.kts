bc54010f08f5a8eb7d37992643463a213c2a6e68
Aurimas Liutikas <aurimas@google.com>
2022-12-02 12:16:59 -0800

    Initial checkin

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("com.example.AppKt")
+}
